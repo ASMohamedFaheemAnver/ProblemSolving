cb10344caceffdae30de08773ab75d2baab7847c
ASMohamedFaheemAnver <jstrfaheem065@gmail.com>
2021-01-25 14:14:49 +0530

    # https://www.hackerrank.com/challenges/drawing-book/problem,

---
+++ src/com/freedom/easy/DrawingBook.java
@@ -0,0 +1,28 @@
+package com.freedom.easy;
+
+import java.util.Scanner;
+
+public class DrawingBook {
+
+    static int pageCount(int n, int p) {
+        int cn;
+
+        int tP = (int) (Math.ceil(((float)n-1)/2) + 1);
+        int pP = (int) (Math.ceil(((float)p-1)/2) + 1);
+
+        if((pP - 1) > (tP - pP)){
+            cn  = tP - pP;
+        }else {
+            cn = pP - 1;
+        }
+        return cn;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int p = scanner.nextInt();
+
+        System.out.println(pageCount(n, p));
+    }
+}
