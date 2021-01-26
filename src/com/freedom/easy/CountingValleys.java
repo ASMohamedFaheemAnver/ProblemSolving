ddf6ac5703e3452a7514736c042a3395b484b4fe
ASMohamedFaheemAnver <jstrfaheem065@gmail.com>
2021-01-26 13:25:52 +0530

    # https://www.hackerrank.com/challenges/counting-valleys/forum,

---
+++ src/com/freedom/easy/CountingValleys.java
@@ -0,0 +1,38 @@
+package com.freedom.easy;
+
+import java.util.Scanner;
+
+public class CountingValleys {
+
+    public static int countingValleys(int steps, String path) {
+        int valley = 0;
+        int e = 0;
+        boolean start = false;
+
+        for (int i = 0; i<steps; i++){
+            if(path.charAt(i) == 'U'){
+                e++;
+                if(e==0 && start){
+                    valley++;
+                    start = false;
+                }
+            }else {
+                e--;
+                if (e<0){
+                    start = true;
+                }
+            }
+        }
+        return valley;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int steps = scanner.nextInt();
+        scanner.nextLine();
+        String path = scanner.nextLine();
+
+        System.out.println(countingValleys(steps, path));
+    }
+}
