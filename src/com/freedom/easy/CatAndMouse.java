b161523b4cbb9c2163f4de00e32e02073b854d0d
ASMohamedFaheemAnver <jstrfaheem065@gmail.com>
2021-01-28 10:19:27 +0530

    # https://www.hackerrank.com/challenges/cats-and-a-mouse/problem,

---
+++ src/com/freedom/easy/CatAndMouse.java
@@ -0,0 +1,36 @@
+package com.freedom.easy;
+
+import java.util.Scanner;
+
+public class CatAndMouse {
+
+    static String catAndMouse(int x, int y, int z) {
+        String res;
+        int xz = Math.abs(z - x);
+        int yz = Math.abs(z - y);
+
+        if(xz > yz){
+            res = "Cat B";
+        }else if(yz > xz){
+            res = "Cat A";
+        }else {
+            res = "Mouse C";
+        }
+
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int q = scanner.nextInt();
+
+        for (int i = 0; i<q; i++){
+            int x = scanner.nextInt();
+            int y = scanner.nextInt();
+            int z = scanner.nextInt();
+            System.out.println(catAndMouse(x, y, z));
+        }
+    }
+}
