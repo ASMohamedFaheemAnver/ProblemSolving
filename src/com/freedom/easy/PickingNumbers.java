60c320f9c574cbbc2db881e6acb04371d740a3c6
ASMohamedFaheemAnver <jstrfaheem065@gmail.com>
2021-02-06 18:26:24 +0530

    # https://www.hackerrank.com/challenges/picking-numbers/problem,

---
+++ src/com/freedom/easy/PickingNumbers.java
@@ -0,0 +1,31 @@
+package com.freedom.easy;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class PickingNumbers {
+    public static int pickingNumbers(List<Integer> a) {
+        final int[] max = {0};
+        int map[] = new int[100];
+        a.forEach(n -> {
+            map[n-1]++;
+        });
+
+        a.forEach(n->{
+            if((map[n-1] + map[n])> max[0]){
+                max[0] = map[n-1] + map[n];
+            }
+        });
+
+        return max[0];
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        scanner.nextLine();
+        List<Integer> a = Arrays.stream(scanner.nextLine().split(" ")).map(Integer::parseInt).collect(Collectors.toList());
+        System.out.println(pickingNumbers(a));
+    }
+}
