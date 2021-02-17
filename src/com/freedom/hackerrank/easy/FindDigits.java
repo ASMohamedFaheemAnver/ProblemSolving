11d5ff48303d497cb0dae33e22601105ba0d2bfd
ASMohamedFaheemAnver <jstrfaheem065@gmail.com>
2021-02-17 18:12:45 +0530

    # https://www.hackerrank.com/challenges/find-digits/problem,

---
+++ src/com/freedom/hackerrank/easy/FindDigits.java
@@ -0,0 +1,30 @@
+package com.freedom.hackerrank.easy;
+
+import java.util.Scanner;
+
+public class FindDigits {
+
+    static int findDigits(int n) {
+        String sN = String.valueOf(n);
+        int divisors = 0;
+        for (int i = 0; i < sN.length(); i++) {
+            try{
+                if ((n % Integer.parseInt(String.valueOf(sN.charAt(i)))) == 0) {
+                    divisors++;
+                }
+            }catch (ArithmeticException e){
+
+            }
+        }
+        return divisors;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
+
+        for (int i = 0; i < t; i++) {
+            System.out.println(findDigits(scanner.nextInt()));
+        }
+    }
+}
