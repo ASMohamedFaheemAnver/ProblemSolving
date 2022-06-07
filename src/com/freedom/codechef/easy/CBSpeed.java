c3f89cbd75e824fd92a56e781a0ab213ece290fe
FreeDoM <jstrfaheem065@gmail.com>
2022-06-07 23:49:48 +0530

    # https://www.codechef.com/submit-v2/CBSPEED?tab=statement

--- src/com/freedom/codechef/easy/CBSpeed.java
+++ src/com/freedom/codechef/easy/CBSpeed.java
@@ -1,12 +1,16 @@
 package com.freedom.codechef.easy;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class CBSpeed {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int thresholdSpeed = scanner.nextInt();
-        int currentSpeed = scanner.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        int thresholdSpeed = Integer.parseInt(stringTokenizer.nextToken());
+        int currentSpeed = Integer.parseInt(stringTokenizer.nextToken());
         if(thresholdSpeed >= currentSpeed) {
             System.out.println("NO");
         }else {
