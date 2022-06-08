923f3e97f6dc404fc7b447de37bc3050d9720f45
FreeDoM <jstrfaheem065@gmail.com>
2022-06-09 01:00:04 +0530

    N^2 Time Solution,

--- src/com/freedom/codechef/medium/FORG.java
+++ src/com/freedom/codechef/medium/FORG.java
@@ -8,13 +8,39 @@ import java.util.StringTokenizer;
 
 public class FORG {
     // Time N^3 | Space 1
-    static int getNumberOfSubstringsWithSpecialCharacter(String string, String specialCharacter){
+    static int getNumberOfSubstringsWithSpecialCharacter(String string, char specialCharacter){
         int numberOfMatches = 0;
         for (int i = 0; i<string.length(); i++){
             for (int j = i; j<string.length(); j++){
-                if(string.substring(i, j+1).contains(specialCharacter)){
+                if(string.substring(i, j+1).contains(String.valueOf(specialCharacter))){
                     numberOfMatches++;
                 }
+            }
+        }
+        return numberOfMatches;
+    }
+
+    static long getNumberOfSubstringsWithSpecialCharacterOptimized(String string, char specialCharacter, int length){
+        long numberOfMatches = 0;
+        for (int i = 0; i<length; i++){
+            for (int j = i; j<length; j++){
+                if(string.charAt(j) == specialCharacter){
+                    numberOfMatches += (length-j);
+                    break;
+                }
+            }
+        }
+        return numberOfMatches;
+    }
+
+    static int getNumberOfSubstringsWithSpecialCharacterAlternativeOptimized(String string, char specialCharacter, int length){
+        int numberOfMatches = 0;
+        int left = 0;
+        for (int i = 0; i<length; i++){
+            left++;
+            if(string.charAt(i) == specialCharacter){
+                numberOfMatches += left*(length-i);
+                left = 0;
             }
         }
         return numberOfMatches;
@@ -24,11 +50,11 @@ public class FORG {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         int testCases = Integer.parseInt(bufferedReader.readLine());
         for (int i = 0; i<testCases; i++){
-            bufferedReader.readLine();
+            int length = Integer.parseInt(bufferedReader.readLine());
             StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
             String string = stringTokenizer.nextToken();
-            String specialCharacter = stringTokenizer.nextToken();
-            System.out.println(getNumberOfSubstringsWithSpecialCharacter(string, specialCharacter));
+            char specialCharacter = stringTokenizer.nextToken().charAt(0);
+            System.out.println(getNumberOfSubstringsWithSpecialCharacterOptimized(string, specialCharacter, length));
         }
     }
 }
